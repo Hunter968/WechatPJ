4fe32a4fc186cc4d7eacd07385a398d8850a02fe
Hunter968 <login007@aliyun.com>
2016-03-29 09:59:59 +0800

    首次提交

---
+++ src/main/java/com/xq/wechat/WechatPjApplication.java
@@ -0,0 +1,12 @@
+package com.xq.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatPjApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xq/wechat/WechatPjApplicationTests.java
@@ -0,0 +1,18 @@
+package com.xq.wechat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WechatPjApplication.class)
+@WebAppConfiguration
+public class WechatPjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
